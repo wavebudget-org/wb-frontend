dcd0b28c42158c6096c28d3f08dd182552cc9e2a
hussain <abdulbaqhussain2018@gmail.com>
2023-04-16 13:02:32 -0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+   <h1>Welcome</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  
+  }
+}
+</script>
