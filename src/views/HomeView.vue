2fe47f28214891e9b92a789b1451eaaa50dc07f7
Yousouph Olosan <depolymathyousouph@gmail.com>
2023-04-30 08:40:12 +0100

    'refactoring'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,7 +14,8 @@
       <button class="join-btn">Join waitlist</button>
     </div>
     <div class="drop-down">
-      <img class="drop-img"
+      <img
+        class="drop-img"
         @click="mobileNavMenu = !mobileNavMenu"
         src="../assets/images/wave-dropdown.png"
         alt=""
@@ -36,7 +37,7 @@
       <p class="body-paragraph">
         Pay now, pay later for items to reach <br />
         other needs that matter, pay back with <br />
-        0% intrest rate
+        0% interest rate
       </p>
     </div>
     <div class="body-image">
@@ -45,10 +46,12 @@
   </section>
   <div class="body-btn">
     <button class="btn-body1">Get started</button>
-    <button class="btn-body2">I have a question</button>
-    <span class="message-logo"
+    <button class="btn-body2">
+      I have a question <img src="../assets/images/message.png" alt="" />
+    </button>
+    <!-- <span class="message-logo"
       ><img src="../assets/images/message.png" alt=""
-    /></span>
+    /></span> -->
   </div>
   <section class="shop-now">
     <article class="grid-shop-now">
@@ -88,46 +91,49 @@
       </div>
     </article>
   </section>
-  <div class="demo-section">
+  <article class="demo-section">
     <h1 class="demo-h1">TRY A DEMO</h1>
     <p class="demo-p">BNPL Calculator</p>
-    <div class="bnpl-calculator">
-      <form class="calculator-form">
-        <div class="form-tag">
-          <label>What is your monthly income?</label>
+    <section class="bnpl_table">
+      <form>
+        <div class="form-control">
+          <label for="monthly_income">What is your monthly income</label>
           <input
             type="text"
             placeholder="This information is important to get your credit limit"
           />
         </div>
-        <div class="form-tag">
-          <label> What item do you want to buy? </label>
-          <input type="text" placeholder="" />
-        </div>
-        <div class="form-tag">
-          <label>What is the item price range?</label>
+        <div class="form-control">
+          <label for="what_you_want_to_buy"
+            >What item do you want to buy?</label
+          >
           <input type="text" />
         </div>
-        <div class="form-tag">
-          <label>Select a payment term</label>
+        <div class="form-control">
+          <label for="price_range">What is the item price range?</label>
           <input type="text" />
         </div>
-        <div class="bnpl-btn">
-          <button class="btn-generate">Generate</button>
+
+        <div class="form-control">
+          <label for="select_payment">Select a payment term</label>
+          <input type="text" />
+        </div>
+        <div class="form-btn">
+          <button class="form-button">Submit</button>
         </div>
       </form>
-
-      <!-- <div class="phone-image"> -->
       <img class="phone-image" src="../assets/images/phone.png" alt="" />
-      <!-- </div> -->
-    </div>
-  </div>
+    </section>
+  </article>
   <div class="how-it-works-section">
     <h1 class="works">HOW IT WORKS</h1>
   </div>
   <div class="grid-how-it-works">
     <div>
-      <img class="how-it-work-img" src="../assets/images/profile.png" alt="" />
+      <div class="how-it-work-img">
+        <img src="../assets/images/profile.png" alt="" />
+      </div>
+
       <h3 class="how-it-work-h3">Registeration</h3>
       <p class="how-it-work-p">
         Create a profile on the WaveBudget platform in less than five minutes
@@ -135,7 +141,10 @@
       </p>
     </div>
     <div>
-      <img class="how-it-work-img" src="../assets/images/cart.png" alt="" />
+      <div class="how-it-work-img">
+        <img src="../assets/images/cart.png" alt="" />
+      </div>
+
       <h3 class="how-it-work-h3">Choose an item to buy</h3>
       <p class="how-it-work-p">
         Select an item on the online WaveMart store or visit any of the
@@ -143,7 +152,9 @@
       </p>
     </div>
     <div>
-      <img class="how-it-work-img" src="../assets/images/payment.png" alt="" />
+      <div class="how-it-work-img">
+        <img src="../assets/images/payment.png" alt="" />
+      </div>
       <h3 class="how-it-work-h3">Select a payment plan</h3>
       <p class="how-it-work-p">
         The item will be delivered immediately while you continue your payment
@@ -151,7 +162,9 @@
       </p>
     </div>
     <div>
-      <img class="how-it-work-img" src="../assets/images/delivery.png" alt="" />
+      <div class="how-it-work-img">
+        <img src="../assets/images/delivery.png" alt="" />
+      </div>
       <h3 class="how-it-work-h3">Delivery and payment</h3>
       <p class="how-it-work-p">
         The item will be delivered immediately while you continue your payment
@@ -159,183 +172,176 @@
       </p>
     </div>
   </div>
-  <div class="join-us">
-    <h1 class="join-us-h1" >Join Us Today</h1>
-  </div>
-  <main class="main-flex">
-    <div class="individual">Individual</div>
-  <div class="business">Bussiness</div>
-  </main>
-  
-  <section class="join-us-flex">
-   <div class="individual-grid-1">
-    <div class="flex-part">
-     <div> <img class="flex-image" src="../assets/images/payment.png" alt=""></div>
-    <div> 
-    <h3 class="flex-part-h3">Convenient Payment </h3>
-    <p class="flex-part-p">WaveBudget offers individuals a hassle free way to purchase items and pay for them over 
-      time. With flexible payment plans
-       and variety of products available.</p>
-    </div>
-    </div>
-      <div class="flex-part-1">
-     <div> <img class="flex-image" src="../assets/images/setting.png" alt=""></div>
-    <div> 
-    <h3 class="flex-part-h3">100% Secure </h3>
-    <p class="flex-part-p">WaveBudget takes your security seriously, using the latest encryption and 
-      security measures to keep your information safe. </p>
-    </div>
-</div>
- <div class="flex-part-1">
-     <div> <img class="flex-image" src="../assets/images/home.png" alt=""></div>
-    <div> 
-    <h3 class="flex-part-h3">Affordable Loan</h3>
-    <p class="flex-part-p">WaveBudget loans are designed to meet the needs of individuals with zero interest
-       rates and repayment terms that fits your budget. </p>
-    </div>
-</div>
-<div class="flex-part">
-     <div> <img class="flex-image" src="../assets/images/profile.png" alt=""></div>
-    <div> 
-    <h3 class="flex-part-h3">Easy To Use</h3>
-    <p class="flex-part-p">With the simple registration process and user friendly interface, 
-      WaveBidget is easy to use.</p>
-    </div>
-</div>
-   </div>
-   <div class="individual-grid-2">
-    <div class="flex-part">
-     <div> <img class="flex-image" src="../assets/images/mountain.png" alt=""></div>
-    <div> 
-    <h3 class="flex-part-h3">Convenient Payment </h3>
-    <p class="flex-part-p">WaveBudget offers individuals a hassle free way to purchase items and pay for them over 
-      time. With flexible payment plans
-       and variety of products available.</p>
-    </div>
-    </div>
-      <div class="flex-part-1">
-     <div> <img class="flex-image" src="../assets/images/speaker.png" alt=""></div>
-    <div> 
-    <h3 class="flex-part-h3">100% Secure </h3>
-    <p class="flex-part-p">WaveBudget takes your security seriously, using the latest encryption and 
-      security measures to keep your information safe. </p>
-    </div>
-</div>
- <div class="flex-part-1">
-     <div> <img class="flex-image" src="../assets/images/human.png" alt=""></div>
-    <div> 
-    <h3 class="flex-part-h3">Affordable Loan</h3>
-    <p class="flex-part-p">WaveBudget loans are designed to meet the needs of individuals with zero interest
-       rates and repayment terms that fits your budget. </p>
-    </div>
-</div>
-<div class="flex-part">
-     <div> <img class="flex-image" src="../assets/images/delivery.png" alt=""></div>
-    <div> 
-    <h3 class="flex-part-h3">Instant Payment</h3>
-    <p class="flex-part-p">With the simple registration process and user friendly interface, 
-      WaveBidget is easy to use.</p>
-    </div>
-</div>
 
-   </div>
+  <section class="join-us">
+    <h1 class="join-us-h1">Join Us Today!</h1>
+
+    <article class="join-us-flex">
+      <h3 class="header">Individuals</h3>
+      <div class="individual-grid-1">
+        <div
+          class="cont"
+          v-for="individual in individuals"
+          :key="individual.title"
+        >
+          <img
+            :src="require(`../assets/images/${individual.img}.png`)"
+            :alt="individual.img"
+          />
+          <div>
+            <h4>{{ individual.title }}</h4>
+            <p>{{ individual.paragraph }}</p>
+          </div>
+        </div>
+      </div>
+    </article>
+    <article class="join-us-flex">
+      <h3 class="header">Businesses</h3>
+      <div class="individual-grid-1">
+        <div class="cont" v-for="business in businesses" :key="business.title">
+          <img
+            :src="require(`../assets/images/${business.img}.png`)"
+            :alt="business.img"
+          />
+          <div>
+            <h4>{{ business.title }}</h4>
+            <p>{{ business.paragraph }}</p>
+          </div>
+        </div>
+      </div>
+    </article>
   </section>
+
   <section>
     <div>
       <h2 class="testimony-h2">Customer Testimony</h2>
     </div>
     <div class="testimony-flex">
-       <div class="testimonies">
-        <h4 class="testimony-h4">Thank you for initiating a Buy Now Pay Later platform.
-Thank you for making it easier to use your platform for solving a pain point.
-The system is currently serving its purpose, a dual use for core profession purpose</h4>
-<p class="testimony-p">From  Lagos</p>
-       </div>
-        <div class="testimonies">
-        <h4 class="testimony-h4">The way you  people are running the business is okay, just keep it up, may Almighty ALLAH help us all,
-           and I really appreciate WaveBudget, WaveBudget is my choice. </h4>
-<p class="testimony-p"> From  Lagos</p>
-       </div>
-        <div class="testimonies">
-        <h4 class="testimony-h4" >WaveBudget made my purchase smooth and satisfying.</h4>
-<p class="testimony-p">From  Lagos</p>
-       </div>
+      <div class="testimonies">
+        <p>
+          Thank you for initiating a Buy Now Pay Later platform. Thank you for
+          making it easier to use your platform for solving a pain point. The
+          system is currently serving its purpose, a dual use for core
+          profession purpose
+        </p>
+        <h4>From Lagos</h4>
+      </div>
+      <div class="testimonies">
+        <p>
+          The way you people are running the business is okay, just keep it up,
+          may Almighty ALLAH help us all, and I really appreciate WaveBudget,
+          WaveBudget is my choice.
+        </p>
+        <h4>From Kaduna</h4>
+      </div>
+      <div class="testimonies">
+        <p>WaveBudget made my purchase smooth and satisfying.</p>
+        <h4>From Lagos</h4>
+      </div>
     </div>
   </section>
-  <section class="app-flex">
-     <div>
-      <h1 class="wave-app">WaveBudget app <br>
-     coming soon.
-     </h1>
-    <div class="app-btn">
-    <button class="btn-body1">Get started</button>
-    <button class="btn-body2">I have a question</button>
-    <span class="message-logo-2"
-      ><img src="../assets/images/message.png" alt=""
-    /></span>
+  <section class="bnpl_table">
+    <div class="coming_soon">
+      <h1 class="h1">WaveBudget app coming soon.</h1>
+      <div class="body-btn">
+        <button class="btn-body1">Join the waitlist</button>
+        <button class="btn-body2">
+          I have a question <img src="../assets/images/message.png" alt="" />
+        </button>
+      </div>
     </div>
-     </div>
-     <div>
-      <img class="phone-image" src="../assets/images/phone-app.png" alt="">
-     </div>
+    <img class="phone-image" src="../assets/images/phone-app.png" alt="" />
   </section>
-  <section class="form-flex">
-    <div>
-    <h3 class="waitlist-h3">Join Waitlist</h3>
-    <form class="last-form">
-     <div>
-       <label>Name</label>
-      <input type="text">
-     </div>
-     <div>
-       <label>Email</label>
-      <input type="text">
-     </div>
-     <div class="form-btn">
-      <button class="form-button">Submit</button>
-     </div>
-    </form>
-    </div>
-    <div>
+  <section>
+    <div class="bnpl_table">
+      <h3 class="waitlist-h3">Join Waitlist</h3>
+      <form>
+        <div class="from-div">
+          <label>Name</label>
+          <input type="text" />
+        </div>
+        <div class="from-div">
+          <label>Email</label>
+          <input type="email" />
+        </div>
+        <span class="form-btn">
+          <button class="form-button">Submit</button>
+        </span>
+      </form>
       <h1 class="take-control">Take total control of your finance.</h1>
     </div>
   </section>
   <div class="FAQ">FAQ</div>
-  <section class="footer-section" >
-  <footer>
-    <div class="footer-message"> 
-     <p class="reach-out">Reach out
-     <span class="footer-span"><img class="reach-out-image" src="../assets/images/g-mail.png" alt=""></span>
-     </p>
-     <div class="footer-img">
-      <img class="message-img" src="../assets/images/twitter.png" alt="">
-     <img class="message-img" src="../assets/images/video.png" alt="">
-     <img class="message-img" src="../assets/images/instagram.png" alt="">
-     <img class="message-img" src="../assets/images/facebook.png" alt="">
-     </div>
+  <footer class="footer__">
+    <div class="first_div">
+      <div class="reach_out">
+        <p>Reach out</p>
+        <span
+          ><img
+            class="reach-out-image"
+            src="../assets/images/g-mail.png"
+            alt=""
+        /></span>
+      </div>
+      <img class="message-img" src="../assets/images/twitter.png" alt="" />
+      <img class="message-img" src="../assets/images/video.png" alt="" />
+      <img class="message-img" src="../assets/images/instagram.png" alt="" />
+      <img class="message-img" src="../assets/images/facebook.png" alt="" />
     </div>
     <div>
       <h3>About product</h3>
-      <p>Updates
-       Beta test
-</p>
+      <p>Updates</p>
+      <p>Beta test</p>
     </div>
-    <div class="footer-resources">
+    <div>
       <h3>Resources</h3>
-      <p>Help center
-       Partners
-        News letters</p>
+      <p>Help center</p>
+      <p>Partners</p>
+      Partners
+      <p>News letters</p>
     </div>
-  </footer>
-  <footer class="l-section">
-    <p class="l2-section">
-       @ Copyright 2023. All rights reserved
-    </p>
+    <p>@ Copyright {{ new Date().getFullYear() }}. All rights reserved</p>
     <p>Terms and Conditions</p>
     <p>Privacy Policy</p>
-    
   </footer>
-  </section>
+  <!-- <section class="footer-section">
+    <footer>
+      <div class="footer-message">
+        <p class="reach-out">
+          Reach out
+          <span class="footer-span"
+            ><img
+              class="reach-out-image"
+              src="../assets/images/g-mail.png"
+              alt=""
+          /></span>
+        </p>
+        <div class="footer-img">
+          <img class="message-img" src="../assets/images/twitter.png" alt="" />
+          <img class="message-img" src="../assets/images/video.png" alt="" />
+          <img
+            class="message-img"
+            src="../assets/images/instagram.png"
+            alt=""
+          />
+          <img class="message-img" src="../assets/images/facebook.png" alt="" />
+        </div>
+      </div>
+      <div>
+        <h3>About product</h3>
+        <p>Updates Beta test</p>
+      </div>
+      <div class="footer-resources">
+        <h3>Resources</h3>
+        <p>Help center Partners News letters</p>
+      </div>
+    </footer>
+    <footer class="l-section">
+      <p class="l2-section">@ Copyright 2023. All rights reserved</p>
+      <p>Terms and Conditions</p>
+      <p>Privacy Policy</p>
+    </footer>
+  </section> -->
 </template>
 
 <script>
@@ -347,7 +353,70 @@ export default {
   data() {
     return {
       mobileNavMenu: false,
+      individuals: [
+        {
+          img: "payment",
+          title: "Convenent Payment",
+          paragraph:
+            "WaveBudget offers individuals a hassle free way to purchase items and pay for them over time. With flexible payment plans and variety of products available.",
+        },
+        {
+          img: "setting",
+          title: "100% Secure",
+          paragraph:
+            "WaveBudget takes your security seriously, using the latest encryption and security measures to keep your information safe. ",
+        },
+        {
+          img: "home",
+          title: "Affordable Loan",
+          paragraph:
+            "WaveBudget loans are designed to meet the needs of individuals with zero interest rates and repayment terms that fits your budget.",
+        },
+        {
+          img: "profile",
+          title: "Easy To Use",
+          paragraph:
+            "With the simple registration process and user friendly interface, WaveBidget is easy to use.",
+        },
+      ],
+      businesses: [
+        {
+          img: "mountain",
+          title: "Increased Sales",
+          paragraph:
+            "By partnering with WaveBudget , businesses can increase their sales by offering customers a convenient way to purchase items and pay for them over time",
+        },
+        {
+          img: "speaker",
+          title: "Brand Awareness",
+          paragraph:
+            " Partnering with WaveBudget can help business increase their brand awareness, reaching a wider audience of potential customers.",
+        },
+        {
+          img: "human",
+          title: "Easy  Onboarding",
+          paragraph:
+            "WaveBudget makes it easy for businesses to onboard and start offering their products through the platform, with no complex setup or integration required.",
+        },
+        {
+          img: "delivery",
+          title: "Instant Payment",
+          paragraph:
+            " With the simple registration process and user friendly interface, WaveBidget is easy to use.",
+        },
+      ],
+      carousel: [
+        { title: "", paragraph: "", img: "" },
+        { title: "", paragraph: "", img: "" },
+        { title: "", paragraph: "", img: "" },
+      ],
     };
+  },
+  methods: {
+    animate() {},
+  },
+  mounted() {
+    this.animate();
   },
 };
 </script>
